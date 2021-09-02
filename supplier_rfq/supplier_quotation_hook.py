d94c9fb99b2d6ba9d0f7bc3148aa3acbe00af79a
ashish-greycube <mr.ashish.shah@gmail.com>
2021-09-02 11:18:53 +0530

    fix: load table

--- supplier_rfq/supplier_quotation_hook.py
+++ supplier_rfq/supplier_quotation_hook.py
@@ -6,7 +6,7 @@ from frappe.utils import add_days, today, nowdate, get_date_str,now
 
 def onload(self,method):
     request_for_quotation=self.items[0].request_for_quotation
-    if self.name and self.docstatus!=2  and request_for_quotation:
+    if self.name and self.docstatus!=2  and request_for_quotation and len(self.supplier_quotation_comparisons)==0:
         filters={
                 'company': self.company, 
                 'from_date': get_date_str(frappe.db.get_value('Request for Quotation', request_for_quotation, 'transaction_date')),
@@ -18,9 +18,13 @@ def onload(self,method):
                 'include_expired': 1
                 }
         columns, data, message, chart_data= execute(filters)
+        updated=False
         if data:
             for row in data:
-                self.append("supplier_quotation_comparisons",row)
-                self.save(ignore_permissions=True)
-                frappe.db.commit()                 
-                frappe.msgprint(msg=_("Supplier quotation comparison is updated."), indicator='green',alert=True)
+                if row and len(row) >0:
+                    self.append("supplier_quotation_comparisons",row)
+                    self.save(ignore_permissions=True)
+                    frappe.db.commit() 
+                    updated=True                
+        if updated==True:
+            frappe.msgprint(msg=_("Supplier quotation comparison is updated."), indicator='green',alert=True)

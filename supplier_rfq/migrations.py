dd93dfd881d8abfc2c884c279b6e7c2a78c60828
ashish-greycube <mr.ashish.shah@gmail.com>
2021-09-08 15:00:24 +0530

    json

--- supplier_rfq/migrations.py
+++ supplier_rfq/migrations.py
@@ -7,13 +7,15 @@ from os.path import join
 
 def after_migrations():
 	supplier_rfq_app_folder_path='/apps/supplier_rfq/supplier_rfq/import_records'
-
+	print('-'*100)
 	if(not frappe.db.exists('Custom Field','Request for Quotation-supplier_comparison_section')):
+		print('custom field picked up')
 		fname="custom_field.json"
 		import_folder_path="{bench_path}/{app_folder_path}".format(bench_path=get_bench_path(),app_folder_path=supplier_rfq_app_folder_path)
 		make_records(import_folder_path,fname)
 
 	if(not frappe.db.exists('Property Setter','Request for Quotation-main-title_field')):
+		print('property setter picked up')
 		fname="property_setter.json"
 		import_folder_path="{bench_path}/{app_folder_path}".format(bench_path=get_bench_path(),app_folder_path=supplier_rfq_app_folder_path)
 		make_records(import_folder_path,fname)
